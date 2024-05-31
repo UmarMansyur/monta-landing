fac90995415fe9093905d78c357395c824825c3e
Muhammad Umar Mansyur <umar.ovie@gmail.com>
2024-05-31 14:17:29 +0700

    push

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,7 +15,7 @@
               </div>
             </div>
             <h4 class="text-xl mt-3 font-bold">Selamat datang di MONTAK</h4>
-            <h1 class="block text-xl font-bold text-gray-800 sm:text-4xl lg:text-5xl lg:leading-tight dark:text-white">
+            <h1 class="block text-xl font-bold text-gray-800 sm:text-4xl lg:text-5xl lg:leading-tight">
               Aplikasi <span class="text-blue-600">Monitoring</span> Tugas Akhir dan Kerja Praktek</h1>
             <p class="mt-3 text-xl text-gray-800 dark:text-neutral-400">
               Solusi terdepan yang dirancang khusus untuk memudahkan proses pendaftaran skripsi mahasiswa dan kerja praktek di Universitas Madura
@@ -74,7 +74,7 @@
             <!-- Stats -->
             <div class="lg:pe-6 xl:pe-12">
               <p class="text-6xl font-bold leading-10 text-blue-600">
-                92%
+                2024
                 <span
                   class="ms-1 inline-flex items-center gap-x-1 bg-gray-200 font-medium text-gray-800 text-xs leading-4 rounded-full py-0.5 px-2">
                   <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
@@ -82,10 +82,12 @@
                     <path
                       d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" />
                   </svg>
-                  +7% this month
+                  New Release
                 </span>
               </p>
-              <p class="mt-2 sm:mt-3 text-gray-500">of U.S. adults have bought from businesses using Space</p>
+              <p class="mt-2 sm:mt-3 text-gray-500">
+
+              </p>
             </div>
             <!-- End Stats -->
           </div>
@@ -129,7 +131,7 @@
         <!-- Grid -->
         <div class="grid md:grid-cols-2 gap-12">
           <div class="lg:w-3/4">
-            <h2 class="text-3xl text-gray-800 font-bold lg:text-4xl dark:text-white">
+            <h2 class="text-3xl text-gray-800 font-bold lg:text-4xl">
               Collaborative tools to design better user experience
             </h2>
             <p class="mt-3 text-gray-800 dark:text-neutral-400">
